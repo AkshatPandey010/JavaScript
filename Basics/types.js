bc2a05778ab7db2a0e9a2b6053ff7fffbd5c7e01
Akshat Pandey <pandeyakshat010@gmail.com>
2023-10-25 15:14:19 +0000

    types and operations

---
+++ Basics/types.js
@@ -0,0 +1,24 @@
+// Basics of convertion
+let score = 18
+// console.log(typeof score)
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+let anyNumber = 91
+let convertedString = String(anyNumber)
+// console.log(convertedString);
+// console.log(typeof convertedString);
+
+// ************Operations*************
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log("Akshat" + " Pandey");
+
+
+
+
+
