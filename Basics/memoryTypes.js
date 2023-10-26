cf86942ccc9f42e1f27c7caa55bf8239ac570bb8
Akshat Pandey <pandeyakshat010@gmail.com>
2023-10-26 15:59:07 +0000

    memory types

---
+++ Basics/memoryTypes.js
@@ -0,0 +1,12 @@
+/* Two types of memories 
+Stack(Primitve), Heap(Reference)
+Stack me primitive data types store hote hai aur agr kuch bhi changes hote hai kisi vairable me to uski
+copy me chnages hote hai 
+and Heap me agr kisi ek bhi jagah pe change hua to wo sare objects ki value change kr dega
+*/
+// Example of Stack 
+// let myWebName = "Pandeyakshat"
+// let myAnotherName = myWebName
+// myAnotherName = "AkshatPandey"
+//console.log(myAnotherName); # Output = AkshatPandey
+//console.log(myWebName); # Output = Pandeyakshat
